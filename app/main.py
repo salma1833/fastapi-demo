9790c89bb56090225877742fb20f607f8cb05416
Salma A <143552849+salma1833@users.noreply.github.com>
2024-04-05 15:40:05 -0400

    Update main.py

--- app/main.py
+++ app/main.py
@@ -3,7 +3,7 @@
 from fastapi import FastAPI
 from typing import Optional
 from pydantic import BaseModel
-from models import Item, Album
+from app.models import Item, Album
 import json
 import os
 import MySQLdb
